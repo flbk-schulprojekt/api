ea5d81eecc3e8fd0516b401c82541d2e244d5338
raymenstereo <talhakeskin55@gmail.com>
2024-05-16 12:45:58 +0200

    Added colums to users

--- database/migrations/2024_05_16_102048_users_add_new_columns.php
+++ database/migrations/2024_05_16_102048_users_add_new_columns.php
@@ -11,10 +11,41 @@ return new class extends Migration
      */
     public function up(): void
     {
+
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('admin')->default(false);
+            $genders = [
+                "unknown",
+                "male",
+                "female",
+                "transgender man",
+                "transgender woman",
+                "non-binary",
+                "genderqueer",
+                "genderfluid",
+                "agender",
+                "bigender",
+                "demiboy",
+                "demigirl",
+                "two-spirit",
+                "intersex",
+                "neutrois",
+                "androgynous",
+                "pangender",
+                "trigender",
+                "gender nonconforming",
+                "maverique",
+                "polygender",
+                "third gender",
+                "aliagender"
+            ];
+
+            $table->boolean('is_admin')->default(false);
             $table->string('firstname')->nullable();
             $table->string('lastname')->nullable();
+            $table->string('profile_picture')->nullable();
+            $table->enum('gender',$genders)->default('unknown');
+            $table->date('dob')->nullable();
+            $table->integer('points')->default(0);
         });
     }
 
